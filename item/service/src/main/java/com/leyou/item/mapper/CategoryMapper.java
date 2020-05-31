8278f7d1b6548cbc84e82657678b7eac894c5c65
flexibility2 <729406307@qq.com>
2020-06-01 01:09:28 +0800

    before brand

---
+++ item/service/src/main/java/com/leyou/item/mapper/CategoryMapper.java
@@ -0,0 +1,15 @@
+package com.leyou.item.mapper;
+
+
+import com.leyou.item.domain.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface CategoryMapper {
+
+    List<Category>selectByPid(@Param("parentId") long pid);
+
+}
---
+++ item/service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.leyou.item.service;
+
+import com.leyou.item.domain.Category;
+import com.leyou.item.mapper.CategoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    public List<Category>queryCategoriesByPid(long pid)
+    {
+        return categoryMapper.selectByPid(pid);
+    }
+
+}
