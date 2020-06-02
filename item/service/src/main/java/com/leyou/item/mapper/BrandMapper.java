a7e26726d56ddeede3e0fc56ea0b52983742fb28
flexibility2 <729406307@qq.com>
2020-06-03 01:08:17 +0800

    before image

---
+++ item/service/src/main/java/com/leyou/item/mapper/BrandMapper.java
@@ -0,0 +1,19 @@
+package com.leyou.item.mapper;
+
+import com.leyou.item.domain.Brand;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface BrandMapper {
+
+    List<Brand>selectLikeByName(@Param("name") String name);
+    List<Brand>selectAll();
+
+    int insertBrandAndCategory(@Param("cid") Long cid,@Param("bid") Long bid);
+
+    int insertBrandById(Brand brand);
+
+}
---
+++ item/service/src/test/java/com/leyou/item/testBrandService.java
@@ -0,0 +1,23 @@
+package com.leyou.item;
+
+import com.leyou.common.domain.PageResult;
+import com.leyou.item.domain.Brand;
+import com.leyou.item.service.BrandService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class testBrandService {
+
+    @Autowired
+    BrandService brandService;
+
+    @Test
+    public void test1()
+    {
+        PageResult<Brand>result = brandService.queryBrandsByPage(null,1,5,null,null);
+        System.out.println(result);
+    }
+
+}
